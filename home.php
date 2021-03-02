bcdba9d5b35f5db004872ddd4cb96ec5293cfba5
dyvision <dlang@dssinc.com>
2021-03-02 14:15:35 -0500

    navbar?

--- home.php
+++ home.php
@@ -6,7 +6,12 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
 
+<?php 
+$header = "<div id='navbar'><h3 class='navbar-item'>What We Grubbin' On</h3><h3 class='navbar-item'>Food</h3><h3 class='navbar-item'>Profile</h3></div>";
+?>
+
 <body>
+    <?php echo $header;?>
     <h1>What We Grubbin' On?</h1>
     <span>Find out what you're going to eat tonight. Make a list of interesting foods and have our app randomly suggest one for tonight</span>
     <div class="g-signin2" data-onsuccess="onSignIn"></div>
