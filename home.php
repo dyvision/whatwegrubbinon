e799de522bd664f65f596f6b1cffd00da8353b4a
dyvision <dlang@dssinc.com>
2021-03-02 14:09:21 -0500

    title

--- home.php
+++ home.php
@@ -1,13 +1,15 @@
 <head>
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-<script src="lib/wwgo.js"></script>
-<link href='style/style.css' rel='stylesheet'>
-<meta name="google-signin-client_id" content="634372968316-6p6nf6j795lbja68pd6q35c74pqjb55s.apps.googleusercontent.com">
-<link 
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <script src="lib/wwgo.js"></script>
+    <link href='style/style.css' rel='stylesheet'>
+    <meta name="google-signin-client_id" content="634372968316-6p6nf6j795lbja68pd6q35c74pqjb55s.apps.googleusercontent.com">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
+
 <body>
-<div class="g-signin2" data-onsuccess="onSignIn"></div>
+    <h1>What We Grubbin' On?</h1>
+    <span>Find out what you're going to eat tonight. Make a list of interesting foods and have our app randomly suggest one for tonight</span>
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
 </body>
 
 <?php
-
