839d5bba3a898c8d8c386ea1ae44714e258ae61f
dyvision <dlang@dssinc.com>
2021-03-02 13:59:21 -0500

    forgot

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
 <head>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
+<script src="lib/wwgo.js"></script>
 <meta name="google-signin-client_id" content="634372968316-6p6nf6j795lbja68pd6q35c74pqjb55s.apps.googleusercontent.com">
 </head>
 <body>
