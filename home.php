20dc6ec499b894e66a7e49a71259df8bedefbd9b
dyvision <dlang@dssinc.com>
2021-03-02 14:21:37 -0500

    space it

--- home.php
+++ home.php
@@ -6,16 +6,18 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
 
-<?php 
+<?php
 $header = "<div id='navbar'><h3 class='navbar-item'>What We Grubbin' On</h3><h3 class='navbar-item'>Food</h3><h3 class='navbar-item'>Profile</h3><div class='g-signin2' data-onsuccess='onSignIn'></div></div>";
 ?>
 
 <body>
-    <?php echo $header;?>
+    <?php echo $header; ?>
     <span class='spacer'></span>
-    <h1>What We Grubbin' On?</h1>
-    <span>Find out what you're going to eat tonight. Make a list of interesting foods and have our app randomly suggest one for tonight</span>
-    
+    <div class='row'>
+        <h1>What We Grubbin' On?</h1>
+        <span>Find out what you're going to eat tonight. Make a list of interesting foods and have our app randomly suggest one for tonight</span>
+    </div>
+
 </body>
 
 <?php
