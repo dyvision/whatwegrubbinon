1d5503c41038ad0d9acfd21024bc9e358c7980ca
dyvision <dlang@dssinc.com>
2021-03-02 14:04:19 -0500

    style

--- home.php
+++ home.php
@@ -1,7 +1,9 @@
 <head>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <script src="lib/wwgo.js"></script>
+<link href='style/style.css' rel='stylesheet'>
 <meta name="google-signin-client_id" content="634372968316-6p6nf6j795lbja68pd6q35c74pqjb55s.apps.googleusercontent.com">
+<link 
 </head>
 <body>
 <div class="g-signin2" data-onsuccess="onSignIn"></div>
