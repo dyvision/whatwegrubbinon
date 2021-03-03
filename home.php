ac6eb26285e93d96b673d899c03121f952c885c6
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-03 02:01:08 -0500

    fuckin ocd

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
     <script src="lib/wwgo.js"></script>
     <link href='style/style.css' rel='stylesheet'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='shortcut icon' type='image/png' href='style/wwgologo.png'/>
+    <link rel='shortcut icon' type='image/png' href='style/wwgologo.png' />
     <title>WWGO</title>
 </head>
 
@@ -22,9 +22,9 @@ use wwgo\visual;
         <div style="#position: absolute; #top: 50%;display: table-cell; vertical-align: middle;">
             <div style="padding:0 3%;#position: relative; #top: -50%">
                 <h1 style='font-size:72px'>What We Grubbin' On?</h1>
-                <h3 style='font-size:30px'>Find out what you're going to eat tonight.</h3>
                 <h3 style='font-size:30px'>Add recipes.</h3>
                 <h3 style='font-size:30px'>Get recommendations.</h3>
+                <h3 style='font-size:30px'>Find out what you're going to eat tonight.</h3>
             </div>
         </div>
     </div>
