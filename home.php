d943ecfd7690f7a25ef8622305daf13ab68e51ba
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-03 01:29:43 -0500

    new image

--- home.php
+++ home.php
@@ -2,6 +2,7 @@
     <script src="lib/wwgo.js"></script>
     <link href='style/style.css' rel='stylesheet'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>WWGO</title>
 </head>
 
 <?php
