36ea5a78232174c2a3d6a2cea4ed8bdbde113b77
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-03 02:15:42 -0500

    mobil3

--- home.php
+++ home.php
@@ -21,10 +21,10 @@ use wwgo\visual;
     <div class='banner' style="display: table; #position: relative; overflow: hidden;">
         <div style="#position: absolute; #top: 50%;display: table-cell; vertical-align: middle;">
             <div style="padding:0 3%;#position: relative; #top: -50%">
-                <h1 style='font-size:72px'>What We Grubbin' On?</h1>
-                <h3 style='font-size:30px'>Add recipes.</h3>
-                <h3 style='font-size:30px'>Get recommendations.</h3>
-                <h3 style='font-size:30px'>Find out what you're going to eat tonight.</h3>
+                <h1 class='banner-h1'>What We Grubbin' On?</h1>
+                <h3 class='banner-h3'>Add recipes.</h3>
+                <h3 class='banner-h3'>Get recommendations.</h3>
+                <h3 class='banner-h3'>Find out what you're going to eat tonight.</h3>
             </div>
         </div>
     </div>
