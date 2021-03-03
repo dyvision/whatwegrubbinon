59ac61698e5767963ff375b19de2287b40ee5e57
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-03 01:01:43 -0500

    ooooo

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@ use wwgo\user;
 
 if (isset($_COOKIE['id']) and isset($_COOKIE['guid']) and isset($_COOKIE['refresh_token'])) {
     $auth = new auth();
-    $token = json_decode($auth->authenticate($COOKIE['refresh_token'], 'refresh_token'), true);
+    $token = json_decode($auth->authenticate($_COOKIE['refresh_token'], 'refresh_token'), true);
     $verify = json_decode($auth->verify($token['access_token']), true);
     if ($verify['guid'] != '') {
         $user = new user($token['access_token'], $verify['id'], $verify['guid'], null);
--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@ use wwgo\visual;
 
 if (isset($_COOKIE['id']) and isset($_COOKIE['guid']) and isset($_COOKIE['refresh_token'])) {
     $auth = new auth();
-    $token = json_decode($auth->authenticate($COOKIE['refresh_token'], 'refresh_token'), true);
+    $token = json_decode($auth->authenticate($_COOKIE['refresh_token'], 'refresh_token'), true);
     $verify = json_decode($auth->verify($token['access_token']), true);
     $user = new user($token['access_token'], $_COOKIE['id'], $_COOKIE['guid'], null);
 } else {
