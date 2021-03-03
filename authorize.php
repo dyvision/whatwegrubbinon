4ff3d4f2a850124002cf23cede909135ace9feb0
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-02 22:26:51 -0500

    ah

---
+++ authorize.php
@@ -0,0 +1,8 @@
+<?php
+
+include('lib/wwgo.php');
+
+use wwgo\auth;
+
+$auth = New auth();
+$auth->authorize();
--- profile.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include('lib/wwgo.php');
-
-use wwgo\auth;
-
-$auth = New auth();
-$auth->authorize();
