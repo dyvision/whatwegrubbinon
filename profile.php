5414307ac8d374148bc4ff2c8e0fac773f938c59
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-03 00:27:35 -0500

    login complete

--- profile.php
+++ profile.php
@@ -4,16 +4,23 @@ include('lib/wwgo.php');
 use wwgo\auth;
 use wwgo\user;
 
-$auth = new auth();
-$token = json_decode($auth->authenticate($_GET['code'], 'authorization_code'), true);
-$verify = json_decode($auth->verify($token['access_token']), true);
-if ($verify['guid'] != '') {
-    $user = new user($token['access_token'],$verify['id'], $verify['guid'],null);
-    $user->pull();
+if (isset($_COOKIE['id']) and isset($_COOKIE['guid'])) {
+    $auth = new auth();
+    $token = json_decode($auth->authenticate($_GET['code'], 'authorization_code'), true);
+    $verify = json_decode($auth->verify($token['access_token']), true);
+    if ($verify['guid'] != '') {
+        $user = new user($token['access_token'], $verify['id'], $verify['guid'], null);
+        $user->pull();
+    } else {
+        $user = new user($token['access_token'], $verify['id'], null, $token['refresh_token']);
+        $user->create();
+        $user->pull();
+    }
+    $user->login();
 } else {
-    $user = new user($token['access_token'],$verify['id'],null,$token['refresh_token']);
-    $user->create();
-    $user->pull();
+    $auth = new auth();
+    $token = json_decode($auth->authenticate($_GET['code'], 'authorization_code'), true);
+    $verify = json_decode($auth->verify($token['access_token']), true);
+    $user = new user($token['access_token'], $_COOKIE['id'], $_COOKIE['guid'], null);
 }
 print_r($user->get());
-$user->login();
