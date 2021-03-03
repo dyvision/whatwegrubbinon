794e8a6051fcb179a9928e0d8bdc631d56d30d34
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-03 00:32:56 -0500

    all together now

--- profile.php
+++ profile.php
@@ -25,6 +25,47 @@ if (isset($_COOKIE['id']) and isset($_COOKIE['guid'])) {
         $user->pull();
     }
     $user->login();
-    
+
 }
-print_r($user->get());
+
+$profile = json_decode($user->get(),true);
+
+?>
+
+<head>
+    <script src="lib/wwgo.js"></script>
+    <link href='style/style.css' rel='stylesheet'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+
+<?php
+if (isset($_COOKIE['id'])) {
+    $header = "<div id='navbar'>
+    <h3 class='navbar-item'>What We Grubbin' On</h3>
+    <h3 class='navbar-item'><a href='Food.php'>Food</a></h3>
+    <h3 class='navbar-item'><a href='profile.php'>Profile</a></h3>
+    <h3 class='navbar-item' onclick='logout();'><a>Logout</a></h3>
+    </div>";
+} else {
+    $header = "<div id='navbar'>
+<h3 class='navbar-item'>What We Grubbin' On</h3>
+<h3 class='navbar-item'><a href='Food.php'>Food</a></h3>
+    <h3 class='navbar-item'><a href='profile.php'>Profile</a></h3>
+<h3 class='navbar-item'><a href='authorize.php'>Login with Google</a></h3>
+</div>";
+}
+?>
+
+<body>
+    <?php echo $header; ?>
+    <span class='spacer'></span>
+    <div class='row'>
+        <image src=<?php echo "'".$profile['image']."'";?>></image>
+        <h3><?php echo $profile['email'] ?></h3>
+        <h3><?php echo $profile['fullname'] ?></h3>
+    </div>
+
+</body>
+
+<?php
+
