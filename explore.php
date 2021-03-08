106d445341364476fe86c4b71d0a9f5e2eaee410
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-08 18:10:12 -0500

    button

--- explore.php
+++ explore.php
@@ -50,6 +50,7 @@ echo $build->header();
                                 <image class='profile' src='<?php echo $food['image'] ?>'></image>
                                 <div class='blockbody'>
                                     <h3><?php echo $food['name'] ?></h3>
+                                    <button class='recipe-button'>Add To My Recipes</button>
                                 </div>
                             </a>
                         </div>
