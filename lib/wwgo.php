f4ad57c067403da787e5fe7608d8366839e41e9d
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-08 19:23:31 -0500

    100%

--- lib/wwgo.php
+++ lib/wwgo.php
@@ -523,7 +523,7 @@ namespace wwgo {
         }
         function timezone()
         {
-            echo '<select>
+            echo '<select width="100%">
             <option timeZoneId="1" gmtAdjustment="GMT-12:00" useDaylightTime="0" value="-12">(GMT-12:00) International Date Line West</option>
             <option timeZoneId="2" gmtAdjustment="GMT-11:00" useDaylightTime="0" value="-11">(GMT-11:00) Midway Island, Samoa</option>
             <option timeZoneId="3" gmtAdjustment="GMT-10:00" useDaylightTime="0" value="-10">(GMT-10:00) Hawaii</option>
