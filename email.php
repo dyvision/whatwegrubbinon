89b242afe8cb3070c164095c6040b17f864f8526
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-08 22:35:02 -0500

    test for now

--- email.php
+++ email.php
@@ -7,4 +7,19 @@ use wwgo\user;
 use wwgo\recipe;
 use wwgo\recommendation;
 
+$outputs = '';
 
+$outputs['night'] = '18:00';
+$outputs['all'] = ['8:00','12:00','18:00'];
+$outputs['mondays'] = '12:00';
+$outputs['morning'] = '8:00';
+$outputs['month'] = '8:00';
+
+
+$rec = new recommendation('113077615898413620126');
+$recs = json_decode($rec->get(),true);
+foreach($recs as $email){
+    $gen = new recommendation($email['id']);
+    $result = json_decode($gen->generate());
+    $gen->send($result['rid'],$email['email']);
+}
