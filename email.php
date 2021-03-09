6ef3479febbc985d517364f04533ba18eb32510c
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-08 22:44:58 -0500

    damn

--- email.php
+++ email.php
@@ -20,6 +20,6 @@ $rec = new recommendation('113077615898413620126');
 $recs = json_decode($rec->get(),true);
 foreach($recs as $email){
     $gen = new recommendation($email['id']);
-    $result = json_decode($gen->generate());
+    $result = json_decode($gen->generate(),true);
     $gen->send($result['rid'],$email['email']);
 }
