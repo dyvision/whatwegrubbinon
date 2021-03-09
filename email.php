8bb1c46b76376575cc8a329cee17909960b2fa40
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-08 23:20:45 -0500

    ok

--- email.php
+++ email.php
@@ -4,6 +4,8 @@ include('lib/wwgo.php');
 
 use wwgo\recommendation;
 
+$times = [];
+
 $outputs = '';
 
 $outputs['night'] = '18';
@@ -16,12 +18,18 @@ $now = gmdate("Y-m-d H").':00';
 $rec = new recommendation('113077615898413620126');
 $recs = json_decode($rec->get(), true);
 foreach ($recs as $email) {
-    #echo $now;
+    $script['script_run'] = $now;
     $hour = ($outputs[$email['type']] + $email['tz']).':00';
-    #echo gmdate("Y-m-d ").$hour;
+    $script['scheduled_time'] = gmdate("Y-m-d ").$hour;
     if ($now == $outputs[$email['type']] + $email['tz']) {
         $gen = new recommendation($email['id']);
         $result = json_decode($gen->generate(), true);
         $gen->send($result['rid'], $email['email']);
+        $script['executed'] = 'yes';
+    }else{
+        $script['executed'] = 'no';
     }
+    array_push($times,$script);
+
 }
+print_r(json_encode($times));
