9ef4b4961618bbd7322d1a5bf8cd7aa24de8b9c4
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-08 21:23:19 -0500

    disable

--- profile.php
+++ profile.php
@@ -45,16 +45,16 @@ echo $build->header();
                     <div class='blockbody'>
                         <h3><?php echo $profile['fullname'] ?></h3>
                         <h3>Get Recommendations</h3>
-                        <form disabled>
+                        <form >
                             <?php $build->timezone(); ?>
-                            <select name='frequency'>
+                            <select disabled name='frequency'>
                                 <option value='all'>Every Meal</option>
                                 <option value='night'>Every Night</option>
                                 <option value='morning'>Every morning</option>
                                 <option value='mondays'>Every Monday</option>
                                 <option value='month'>Once a month</option>
                             </select>
-                            <button>Update Frequency</button>
+                            <button disabled>Update Frequency</button>
                         </form>
                         <form action="https://<?php echo $u ?>:<?php echo $p ?>@whatwegrubbinon.com/api/recipe" method='POST'>
                             <input type='text' name='name' placeholder='Recipe Name'>
