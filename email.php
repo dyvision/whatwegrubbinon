45c081aed7b61edd14a5d31a77a5bb1375dbb0b9
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-08 23:06:10 -0500

    let's see

--- email.php
+++ email.php
@@ -7,16 +7,20 @@ use wwgo\recommendation;
 $outputs = '';
 
 $outputs['night'] = '18:00';
-$outputs['all'] = ['8:00','12:00','18:00'];
-$outputs['mondays'] = '12:00';
+$outputs['mondays'] = '8:00';
 $outputs['morning'] = '8:00';
 $outputs['month'] = '8:00';
 
+$now = gmdate("Y-m-d H").':00';
 
 $rec = new recommendation('113077615898413620126');
-$recs = json_decode($rec->get(),true);
-foreach($recs as $email){
-    $gen = new recommendation($email['id']);
-    $result = json_decode($gen->generate(),true);
-    $gen->send($result['rid'],$email['email']);
+$recs = json_decode($rec->get(), true);
+foreach ($recs as $email) {
+    echo $now;
+    echo gmdate("Y-m-d ").$outputs[$email['type']] + $email['tz'];
+    if ($now == $outputs[$email['type']] + $email['tz']) {
+        $gen = new recommendation($email['id']);
+        $result = json_decode($gen->generate(), true);
+        $gen->send($result['rid'], $email['email']);
+    }
 }
